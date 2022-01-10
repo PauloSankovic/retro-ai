80ef63509406d72ce2915e425d5c5aa3bb876c9e
Herkul Herkulic <sankovic.paulo@gmail.com>
2022-01-10 02:50:52 +0100

    Add CartPole python notebook

--- playground/cart-pole/RandomAgent.py
+++ playground/cart-pole/RandomAgent.py
@@ -2,5 +2,5 @@ class RandomAgent:
     def __init__(self, env):
         self.action_space = env.action_space
 
-    def get_action(self):
-        return self.action_space.np_random.choice(self.action_space.n)
+    def get_action(self, state):
+        return self.action_space.np_random.randint(self.action_space.n)
