1ea5477f217a80a85883862e98eb0b9df7b84bb1
Paulo <sankovic.paulo@gmail.com>
2022-05-06 22:44:17 +0200

    Fix logging, rename module to serialization

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 from .log import setup_logger
-from .model import load_model, save_model
+from .serialization import load_model, save_model
--- utils/log.py
+++ utils/log.py
@@ -3,3 +3,4 @@ import logging
 
 def setup_logger(level: int = logging.INFO):
     logging.basicConfig(level=level, format='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+    return logging.getLogger(__name__)
