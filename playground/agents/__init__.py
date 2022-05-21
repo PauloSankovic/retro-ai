fd5589113bead6654aac94534c3323f8a2f35829
Paulo <sankovic.paulo@gmail.com>
2022-05-22 01:00:02 +0200

    Register actor critic agent

--- playground/agents/__init__.py
+++ playground/agents/__init__.py
@@ -2,3 +2,5 @@ from .aStarAgent import AStarAgent
 from .basicQAgent import BasicQAgent
 from .dummyAgent import DummyAgent
 from .randomAgent import RandomAgent
+
+from .acAgent import ActorCriticAgent
