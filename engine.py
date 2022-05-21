187cebba2adf5398a8c597d8ae66dadb8823e7ab
Paulo <sankovic.paulo@gmail.com>
2022-05-22 00:40:02 +0200

    Fix actor critic agent

--- engine.py
+++ engine.py
@@ -2,9 +2,8 @@ import logging
 import time
 
 import gym
-from gym.core import Env
-
 from IPython.display import clear_output
+from gym.core import Env
 
 logger = logging.getLogger(__name__)
 
@@ -14,46 +13,54 @@ def instantiate(environment_id: str, **kwargs) -> Env:
     return gym.make(environment_id, **kwargs)
 
 
-def run(env: Env, agent, stop_callback, verbose: bool = False, **kwargs):
+def run(env: Env, agent, optimizer=None, stop_callback=None, verbose: bool = False, **kwargs):
     _clear_output = kwargs.get('clear_output', False)
 
-    cumulative_reward = 0
+    running_reward = env.spec.reward_threshold * 0.01
     episode = 0
     while True:
         if _clear_output:
             clear_output(wait=True)
 
         reward = run_episode(env, agent, kwargs.get('train', False), kwargs.get('render', True),
-                             kwargs.get('timeout', 0))
-        cumulative_reward += reward
+                             kwargs.get('remember_rewards', False), kwargs.get('timeout', 0))
+        running_reward = 0.05 * reward + 0.95 * running_reward
 
-        if verbose and (episode % 10 == 0 or episode == 999):
-            logger.info(f"Episode {episode} -> Reward {reward}")
+        if optimizer:
+            agent.evaluate(optimizer)
+
+        if verbose and episode % 10 == 0:
+            logger.info(f"Episode {episode} -> Reward {reward} (running reward: {running_reward})")
 
         episode += 1
-        if episode >= 1000:
+        if running_reward > env.spec.reward_threshold:
+            logger.info(f"Solved after {episode} episodes -> {running_reward}")
             break
     logger.info("Terminating")
 
 
-def run_episode(env: Env, agent, train: bool = False, render: bool = True, timeout: float = 0) -> float:
+def run_episode(env: Env, agent, train: bool = False, render: bool = True, remember_rewards: bool = False,
+                timeout: float = 0) -> float:
     state = env.reset()
 
     total_reward = 0.0
     done = False
     while not done:
         if train:
-            action = agent.get_action(state, train)
+            action = agent.get_action(state, True)
             next_state, reward, done, info = env.step(action)
-            agent.train(state, action, next_state, reward, info, done)
+            # agent.train(state, action, next_state, reward, info, done)
         else:
-            action = agent.get_action(state)
+            action = agent.get_action(state, False)
             next_state, reward, done, info = env.step(action)
 
         total_reward += reward
         state = next_state
         if render:
             env.render()
+        if remember_rewards:
+            agent.rewards.append(reward)
+
         time.sleep(timeout)
 
     return total_reward
--- playground/agents/acAgent.py
+++ playground/agents/acAgent.py
@@ -1,7 +1,9 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from gym.core import Env
+from torch.distributions import Categorical
 
 
 class ActorCriticAgent(nn.Module):
@@ -15,18 +17,15 @@ class ActorCriticAgent(nn.Module):
 
         self.gamma = kwargs.pop('gamma', 0.99)
 
-        self.layers = []
-        for i in range(1, len(sizes)):
-            self.layers.append(nn.Linear(in_features=sizes[i - 1], out_features=sizes[i], bias=True))
-        self.actor = nn.Linear(in_features=sizes[-1], out_features=output_size, bias=True)
-        self.critic = nn.Linear(in_features=sizes[-1], out_features=1, bias=True)
+        self.fc1 = nn.Linear(in_features=input_size, out_features=128, bias=True)
+        self.actor = nn.Linear(in_features=128, out_features=output_size, bias=True)
+        self.critic = nn.Linear(in_features=128, out_features=1, bias=True)
 
         self.actions = []
         self.rewards = []
 
-    def forward(self, x, ):
-        for layer in self.layers:
-            x = F.relu(layer(x))
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
 
         action_probability = F.softmax(self.actor(x), dim=-1)
         state_value = self.critic(x)
@@ -38,12 +37,14 @@ class ActorCriticAgent(nn.Module):
         with torch.set_grad_enabled(train):
             action_probability, state_value = self.forward(state)
 
-        action = torch.argmax(action_probability)
+        m = Categorical(action_probability)
+        action = m.sample()
+
         if train:
-            self.actions.append((action_probability[action], state_value))
+            self.actions.append((m.log_prob(action), state_value))
         return action.item()
 
-    def evaluate(self):
+    def evaluate(self, optimizer):
         cum_reward = 0
         expected_returns = []
         for reward in self.rewards[::-1]:
@@ -51,70 +52,21 @@ class ActorCriticAgent(nn.Module):
             expected_returns.insert(0, cum_reward)
 
         expected_returns = torch.tensor(expected_returns)
-        expected_returns = (expected_returns - expected_returns.mean()) / (expected_returns.std() - np.finfo(np.float32).eps.item())
+        expected_returns = (expected_returns - expected_returns.mean()) / (expected_returns.std() + np.finfo(np.float32).eps.item())
 
         actor_losses = []
         critic_losses = []
         for (action_prob, state_value), reward in zip(self.actions, expected_returns):
             advantage = reward - state_value.item()
 
-            actor_losses.append(-torch.log(action_prob) * advantage)
-            critic_losses.append(F.smooth_l1_loss(state_value, torch.tensor(reward)))
+            actor_losses.append(-action_prob * advantage)
+            critic_losses.append(F.smooth_l1_loss(state_value, torch.tensor([reward])))
 
         optimizer.zero_grad()
-        loss = sum(actor_losses) + sum(critic_losses)
+        loss = torch.stack(actor_losses).sum() + torch.stack(critic_losses).sum()
 
         loss.backward()
         optimizer.step()
 
         del self.rewards[:]
         del self.actions[:]
-
-
-import engine
-import envs
-import logging
-import torch.optim as optim
-import numpy as np
-
-logger = logging.getLogger(__name__)
-
-env = engine.instantiate(envs.CART_POLE)
-
-agent = ActorCriticAgent(env)
-
-optimizer = optim.Adam(agent.parameters(), lr=3e-2)
-
-env.seed(42)
-torch.manual_seed(42)
-
-
-def run_epoch(render):
-    state = env.reset()
-
-    epoch_reward = 0.0
-    done = False
-    while not done:
-        action = agent.get_action(state, True)
-        next_state, reward, done, info = env.step(action)
-        agent.rewards.append(reward)
-        epoch_reward += reward
-        state = next_state
-        if render:
-            env.render()
-    return epoch_reward
-
-
-cumulative_reward = 0
-epoch = 0
-while True:
-    epoch_reward = run_epoch(render=False)
-    cumulative_reward = 0.05 * epoch_reward + (1 - 0.05) * cumulative_reward
-    print(f"Episode {epoch} -> Reward {epoch_reward}")
-    epoch += 1
-    agent.evaluate()
-    if cumulative_reward > env.spec.reward_threshold:
-        print("Solved! Running reward is now {} and the last episode runs to {} reward!".format(cumulative_reward, epoch_reward))
-        break
-
-logger.info("Terminating")
