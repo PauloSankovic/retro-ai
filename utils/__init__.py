4ef9b9bbc5ec661e12f41f8eebf34219637d611f
psankovic <sankovic.paulo@gmail.com>
2022-05-27 22:56:39 +0200

    Add save and load state dict methods

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 from .log import setup_logger
-from .serialization import load_model, save_model
+from .serialization import load_model, save_model, load_state_dict, save_state_dict
--- utils/serialization.py
+++ utils/serialization.py
@@ -1,6 +1,7 @@
 import logging
 import os
-import pickle
+import torch
+import torch.nn as nn
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
@@ -19,12 +20,28 @@ def save_model(model, base_dir: str = "models", **kwargs) -> None:
     with open(f"{path}.pickle", "wb") as f:
         try:
             logger.info(f"Saving model at location {os.path.realpath(f.name)}")
-            pickle.dump(model, f)
+            torch.save(model, f)
         except Exception as e:
             logger.error(f"Error while saving model {model.name}: {e}")
+
+
+def save_state_dict(model: nn.Module, base_dir: str = "models", **kwargs) -> None:
+    path = create_path(base_dir, model.name + '-state', kwargs)
+    with open(f"{path}.pickle", "wb") as f:
+        try:
+            logger.info(f"Saving model state dictionary at location {os.path.realpath(f.name)}")
+            torch.save(model.state_dict(), f)
+        except Exception as e:
+            logger.error(f"Error while saving model state dictionary {model.name}: {e}")
 
 
 def load_model(model_name: str, base_dir: str = "models", **kwargs) -> object:
     path = create_path(base_dir, model_name, kwargs)
     with open(f"{path}.pickle", "rb") as f:
-        return pickle.load(f)
+        return torch.load(f)
+
+
+def load_state_dict(model_name: str, base_dir: str = "models", **kwargs) -> object:
+    path = create_path(base_dir, model_name + '-state', kwargs)
+    with open(f"{path}.pickle", "rb") as f:
+        return torch.load(f)
