2b4614896cb384ad07c328cc397f2814c2ad8243
psankovic <sankovic.paulo@gmail.com>
2022-05-29 20:25:27 +0200

    Fully implement fc and cnn torch layers

--- playground/networks/basic.py
+++ playground/networks/basic.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 from .structures import CnnStructure
@@ -19,29 +20,27 @@ def fc(fc_dims: list[int]) -> nn.Sequential:
     return net
 
 
-def conv2d_size_out(size, kernel, stride):
-    w = (size[0] - (kernel - 1) - 1) // stride + 1
-    h = (size[1] - (kernel - 1) - 1) // stride + 1
-    return w, h
-
-
-def cnn(cnn_structure: list[CnnStructure], fc_hidden_dims: list[int], out_dim: int) -> nn.Sequential:
+def cnn(observation_space, cnn_structure: list[CnnStructure], fc_hidden_dims: list[int], out_dim: int) -> nn.Sequential:
     cnn_layers = []
-    params = 0
     for i, structure in enumerate(cnn_structure):
         conv = nn.Conv2d(
             in_channels=structure.in_channels,
             out_channels=structure.out_channels,
             kernel_size=structure.kernel_size,
             stride=structure.stride,
-            padding=structure.padding
+            padding=structure.padding,
+            bias=True
         )
-        params = (params, structure.kernel_size, structure.stride)
+
         cnn_layers.append(conv)
         if i != len(cnn_structure) - 1:
             cnn_layers.append(nn.ReLU())
 
-    fc_layers = build_fc([params, *fc_hidden_dims, out_dim])
+    with torch.no_grad():
+        seq = nn.Sequential(*cnn_layers, nn.Flatten())
+        tensors = seq(torch.tensor(observation_space.sample()[None]).float()).shape[1]
+
+    fc_layers = build_fc([tensors, *fc_hidden_dims, out_dim])
 
     net = nn.Sequential(*cnn_layers, nn.Flatten(), *fc_layers)
     return net
--- playground/networks/structures.py
+++ playground/networks/structures.py
@@ -1,5 +1,5 @@
 class CnnStructure:
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int = 0):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = (kernel_size, kernel_size)
